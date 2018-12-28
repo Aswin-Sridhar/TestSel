91be4ea88564c74febb911d81b1b42bf761c5053
Aswin-Sridhar <45232374+Aswin-Sridhar@users.noreply.github.com>
2018-12-28 11:53:44 +0530

    Update SampleClassTwo.java

--- TestRepoJenkins/SampleReports/src/main/java/com/coda/SampleProj/SampleReports/SampleClassTwo.java
+++ TestRepoJenkins/SampleReports/src/main/java/com/coda/SampleProj/SampleReports/SampleClassTwo.java
@@ -28,7 +28,7 @@ public class SampleClassTwo {
 		ExtentTest test = extent.createTest("Redbus", "Launch Redbus website");
 		
 		
-			URL local = new URL("http://10.0.0.143:4444/wd/hub");
+			URL local = new URL("http://abcd:4444/wd/hub");
 			WebDriver driver = new RemoteWebDriver(local, DesiredCapabilities.chrome());
 		
 		//System.setProperty("webdriver.chrome.driver","/Users/aswin/Desktop/chromedriver");
