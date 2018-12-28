de463c623b8c5b1c24325c648428dbed7d1d0285
Aswin-Sridhar <45232374+Aswin-Sridhar@users.noreply.github.com>
2018-12-28 09:09:14 +0530

    Update AddReportDocker.java

--- TestRepoJenkins/SampleReports/src/main/java/com/coda/SampleProj/SampleReports/AddReportDocker.java
+++ TestRepoJenkins/SampleReports/src/main/java/com/coda/SampleProj/SampleReports/AddReportDocker.java
@@ -28,7 +28,7 @@ public class AddReportDocker {
 		ExtentTest test = extent.createTest("IRCTC", "Launch IRCTC website");
 		
 		
-			URL local = new URL("http://localhost:4444/wd/hub");//changing abcd to http://54.187.105.230 for swarm experiment
+			URL local = new URL("http://abcd:4444/wd/hub");
 			WebDriver driver = new RemoteWebDriver(local, DesiredCapabilities.chrome());
 		
 		//System.setProperty("webdriver.chrome.driver","/Users/aswin/Desktop/chromedriver");
